65aa6197ad8c6b83f3a9bef0f257084fa2421039
ian oliver <7309792+Ian-Oliver@users.noreply.github.com>
2018-09-25 13:08:51 -0400

    Add files via upload

---
+++ js/boardGameGeek.js
@@ -0,0 +1,23 @@
+$.ajax({
+	url:"https://bgg-json.azurewebsites.net/collection/chaings"
+}).done(function(data){
+	var game = "";
+	$.each($.parseJSON(data), function(key, value){
+		if(value.owned){
+			
+			var name = "<p>" + value.name + "</p>";
+			var img = "<img src = '" + value.thumbnail + "' \>"; 
+			var html ="<div class='row' style='border: 1px solid black'><div class='col-md-12'>";
+			html +="<div class='row'><div class='col-md-12'>";
+			html += name;
+			html += "</div></div>";
+			html +="<div class='row'><div class='col-md-12'>";
+			html += img;
+			html += "</div></div>";
+			html += "</div></div>";
+			$("#listOfGames").append(html);
+		}
+		
+	});
+	
+});
