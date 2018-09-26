ddd45f2f6ef92a0af1f483b0d32defa97f11333e
ian oliver <7309792+Ian-Oliver@users.noreply.github.com>
2018-09-26 12:37:55 -0400

    Add files via upload

--- js/boardGameGeek.js
+++ js/boardGameGeek.js
@@ -7,12 +7,16 @@ $.ajax({
 			
 			var name = "<p>" + value.name + "</p>";
 			var img = "<img src = '" + value.thumbnail + "' \>"; 
+			var players = "<p>Players: " + value.minPlayers + "-" + value.maxPlayers; 
 			var html ="<div class='row' style='border: 1px solid black'><div class='col-md-12'>";
 			html +="<div class='row'><div class='col-md-12'>";
 			html += name;
 			html += "</div></div>";
 			html +="<div class='row'><div class='col-md-12'>";
 			html += img;
+			html += "</div></div>";
+			html +="<div class='row'><div class='col-md-12'>";
+			html += players;
 			html += "</div></div>";
 			html += "</div></div>";
 			$("#listOfGames").append(html);
